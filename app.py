304bc25f356865141d6a06e412f9869715693796
Ammarah Islam <ra@bu.edu>
2024-03-04 21:44:11 -0500

    Added code for Google login

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, url_for, redirect, session
+from authlib.integrations.flask_client import OAuth
+from authlib.common.security import generate_token
 import requests
 
 app = Flask(__name__)
+app.secret_key = "AIzaSyDpTkljTjVCmddpR1AL7hFktdGJ5YGA_x8"
+oauth = OAuth(app)
 
 @app.route('/random_recipe')
 def random_recipe():
@@ -22,6 +26,42 @@ def random_recipe():
         })
     else:
         return jsonify({"error": "Failed to fetch recipe"}), response.status_code
+    
+@app.route('/')
+def index():
+    return 'Welcome to Flask OAuth Google Login! <a href="/google/">Login with Google</a>'
+
+@app.route('/google/')
+def google():
+
+    GOOGLE_CLIENT_ID = '14130293736-68kd4k5bmnvddmth3ah9d5q67mkkhb5t.apps.googleusercontent.com'
+    GOOGLE_CLIENT_SECRET = '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'
+
+    CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+    oauth.register(
+        name='google',
+        client_id=GOOGLE_CLIENT_ID,
+        client_secret=GOOGLE_CLIENT_SECRET,
+        server_metadata_url=CONF_URL,
+        client_kwargs={
+            'scope': 'openid email profile'
+        }
+    )
+
+    # Redirect to google_auth function
+    redirect_uri = url_for('google_auth', _external=True)
+    print(redirect_uri)
+    session['nonce'] = generate_token()
+    return oauth.google.authorize_redirect(redirect_uri)
+
+@app.route('/google/auth/')
+def google_auth():
+    token = oauth.google.authorize_access_token()
+    user = oauth.google.parse_id_token(token, nonce=session['nonce'])
+    session['user'] = user
+    print(" Google User ", user)
+    return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
